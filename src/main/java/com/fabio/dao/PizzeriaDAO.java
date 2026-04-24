8ef5ca7ee82f879a1fbafacf787a81e2b06a55b8
milanofabiojr <milanofabiojr@gmail.com>
2026-04-24 10:33:37 +0200

    aggiornamenti vari

--- src/main/java/com/fabio/dao/PizzeriaDAO.java
+++ src/main/java/com/fabio/dao/PizzeriaDAO.java
@@ -16,123 +16,125 @@ import java.util.List;
 
 public class PizzeriaDAO {
 
-    public Utente login(String username, String password)  {
-        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
-        try {
-        	Utente u;
-        	 u = em.createQuery(
-                "SELECT u FROM Utente u WHERE u.username=:u AND u.password=:p",
-                Utente.class)
-                .setParameter("u", username)
-                .setParameter("p", password)
-                .getSingleResult();
-        	 return u;
-        } catch (NoResultException e) {
-        	return null;
-        }
-    }
-    
-    public List<Utente> getAllUtenti(){
-    	EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
-        return em.createQuery("SELECT u FROM Utente u", Utente.class).getResultList();
-    }
+	public Utente login(String username, String password) {
+		EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+		try {
+			Utente u;
+			u = em.createQuery("SELECT u FROM Utente u WHERE u.username=:u AND u.password=:p", Utente.class)
+					.setParameter("u", username).setParameter("p", password).getSingleResult();
+			return u;
+		} catch (NoResultException e) {
+			return null;
+		} finally {
+			em.close();
+		}
+	}
 
-    public List<Pizza> findAllbyUtenteId(int utenteId) {
-    	List<Pizza> pizze;
-        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
-        TypedQuery<Pizza> q = em.createQuery("SELECT DISTINCT p FROM Pizza p " +
-                "LEFT JOIN FETCH p.ingredienti " +
-                "JOIN FETCH p.impasto " +
-                "WHERE p.utente.id = :id",Pizza.class);
-        q.setParameter("id", utenteId);
-        pizze = q.getResultList();
-        em.close();
-        return pizze;
-    }
+	public List<Utente> getAllUtenti() {
+		EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+		try {
+			return em.createQuery("SELECT u FROM Utente u", Utente.class).getResultList();
+		} catch (NoResultException e) {
+			return null;
+		} finally {
+			em.close();
+		}
+	}
 
-    public void save(Pizza pizza) {
-        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
-        em.getTransaction().begin();
-        em.persist(pizza);
-        em.getTransaction().commit();
-        em.close();
-    }
-    
-    public Pizza addPizza(Pizza pizza) {
-        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
-        em.getTransaction().begin();
-        em.persist(pizza);
-        em.getTransaction().commit();
-        em.close();
-        return pizza;
-    }
+	public List<Pizza> findAllbyUtenteId(int utenteId) {
+		List<Pizza> pizze;
+		EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+		TypedQuery<Pizza> q = em.createQuery("SELECT DISTINCT p FROM Pizza p " + "LEFT JOIN FETCH p.ingredienti "
+				+ "JOIN FETCH p.impasto " + "WHERE p.utente.id = :id", Pizza.class);
+		q.setParameter("id", utenteId);
+		pizze = q.getResultList();
+		em.close();
+		return pizze;
+	}
 
-    public void update(Pizza p) {
-        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
-        em.getTransaction().begin();
-        em.merge(p);
-        em.getTransaction().commit();
-        em.close();
-    }
-    
-    public Pizza updatePizza(Pizza p) {
-        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
-        em.getTransaction().begin();
-        em.merge(p);
-        em.getTransaction().commit();
-        em.close();
-        return p;
-    }
+	public void save(Pizza pizza) {
+		EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+		em.getTransaction().begin();
+		em.persist(pizza);
+		em.getTransaction().commit();
+		em.close();
+	}
 
-    public void delete(int id) {
-        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
-        em.getTransaction().begin();
-        Pizza p = em.find(Pizza.class, id);
-        if (p != null) {
-        em.remove(p);
-        }
-        em.getTransaction().commit();
-        em.close();
-    }
-    
-    public void deletePizza(int id) {
-        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
-        em.getTransaction().begin();
-        Pizza p = em.find(Pizza.class, id);
-        if (p != null) {
-        em.remove(p);
-        }
-        em.getTransaction().commit();
-        em.close();
-    }
-    
-    public Pizza findPizza(int id) {
-        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
-        return em.find(Pizza.class, id);
-    }
+	public Pizza addPizza(Pizza pizza) {
+		EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+		em.getTransaction().begin();
+		em.persist(pizza);
+		em.getTransaction().commit();
+		em.close();
+		return pizza;
+	}
 
-    public List<Impasto> findAllImpasti() {
-    	EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
-        return em.createQuery("SELECT i FROM Impasto i", Impasto.class).getResultList();
-    }
-    
-    public List<Pizza> findAllPizze() {
-    	EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
-        return em.createQuery("SELECT p FROM Pizza p", Pizza.class).getResultList();
-    }
+	public void update(Pizza p) {
+		EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+		em.getTransaction().begin();
+		em.merge(p);
+		em.getTransaction().commit();
+		em.close();
+	}
 
-    public List<Ingrediente> findAllIngredienti() {
-    	EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
-        return em.createQuery("SELECT i FROM Ingrediente i", Ingrediente.class).getResultList();
-    }
+	public Pizza updatePizza(Pizza p) {
+		EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+		em.getTransaction().begin();
+		em.merge(p);
+		em.getTransaction().commit();
+		em.close();
+		return p;
+	}
 
-    public Impasto findImpasto(int id) {
-    	EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
-        return em.find(Impasto.class, id);
-    }
+	public void delete(int id) {
+		EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+		em.getTransaction().begin();
+		Pizza p = em.find(Pizza.class, id);
+		if (p != null) {
+			em.remove(p);
+		}
+		em.getTransaction().commit();
+		em.close();
+	}
 
-    public Ingrediente findIngrediente(int id) {
-    	EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
-        return em.find(Ingrediente.class, id);
-    }
+	public void deletePizza(int id) {
+		EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+		em.getTransaction().begin();
+		Pizza p = em.find(Pizza.class, id);
+		if (p != null) {
+			em.remove(p);
+		}
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	public Pizza findPizza(int id) {
+		EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+		return em.find(Pizza.class, id);
+	}
+
+	public List<Impasto> findAllImpasti() {
+		EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+		return em.createQuery("SELECT i FROM Impasto i", Impasto.class).getResultList();
+	}
+
+	public List<Pizza> findAllPizze() {
+		EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+		return em.createQuery("SELECT p FROM Pizza p", Pizza.class).getResultList();
+	}
+
+	public List<Ingrediente> findAllIngredienti() {
+		EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+		return em.createQuery("SELECT i FROM Ingrediente i", Ingrediente.class).getResultList();
+	}
+
+	public Impasto findImpasto(int id) {
+		EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+		return em.find(Impasto.class, id);
+	}
+
+	public Ingrediente findIngrediente(int id) {
+		EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+		return em.find(Ingrediente.class, id);
+	}
 }
